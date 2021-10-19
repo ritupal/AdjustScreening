6acaf2fd477c55cb9f5e234d3c8e76972bffbb81
ritu pal <ritugrd4@gmail.com>
2021-10-19 11:37:08 +0530

    Basic skeleton added

---
+++ Screening/Scenes/Home/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Screening
+//
+//  Created by Kumari Ritu Pal on 19/10/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
